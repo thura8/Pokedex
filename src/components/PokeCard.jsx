8585e718a9188eaaa31e6285180de9d238c9ac68
ThuRa Lynn Htun <lynnhtunthura8@gmail.com>
2024-12-17 15:44:34 +0630

    Add small images stats and moves to the PokeCard

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -9,6 +9,14 @@ export default function PokeCard({selectedPokemon}) {
 
   const {name,height,abilities,stats,types,moves,sprites} = data || {}
 
+  const imgList = Object.keys(sprites || {}).filter(val =>{
+
+    if(!sprites[val]) return false
+    if(['versions','other'].includes(val)) return false
+
+    return true
+  })
+
   useEffect(() => {
     
     if (loading || !localStorage) return
@@ -81,6 +89,46 @@ if(loading || !data){
         ))}
       </div>
 
+      <img className='default-img' src={'/pokemon/'+ getFullPokedexNumber(selectedPokemon)+'.png'}
+      
+      alt={`${name}-large-img`}
+      />
+
+      <div className='img-container'>
+        {imgList.map((spriteUrl,spriteIndex)=>{
+
+          const imgUrl = sprites[spriteUrl]
+
+          return(
+            <img key={spriteIndex} src={imgUrl} alt={`${name}-img-${spriteUrl}`} />
+          )
+})}
+      </div>
+
+      <h3>Stats</h3>
+      <div className='stats-card'>
+        {stats.map((statObj,statIndex)=>{
+          const {stat,base_stat} = statObj
+          return(
+            <div key={statIndex} className='stat-item'>
+              <p>{stat?.name.replaceAll('-',' ')}</p>
+              <h4>{base_stat}</h4>
+            </div>
+          )
+        })}
+      </div>
+      <h3>Moves</h3>
+      <div className='pokemon-move-grid'>
+        {moves.map((moveObj,moveIndex)=>{
+          return(
+            <button className='button-card pokemon-move' key={moveIndex}
+              onClick={()=>{}}
+            >
+              <p>{moveObj?.move?.name.replaceAll('-',' ')}</p>
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
